a6e66b396349d083619cb43a2bde1212051b2c4b
Isak Makra <89833141+IsakMakra@users.noreply.github.com>
2024-03-05 17:34:49 +0100

    manipulated dataset

--- js/filtering.js
+++ js/filtering.js
@@ -4,10 +4,43 @@
     let dataset = await d3.csv( "vgsales.csv" )
     let platforms = [];
 
-    dataset.forEach(e => {
-        if(!platforms.includes(e.Platform)) {
-            platforms.push(e.Platform);
+    dataset.forEach(object => {
+        if(!platforms.includes(object.Platform)) {
+            platforms.push(object.Platform);
         }
     })
 
+    let unique_names = [];
+    let updated_dataset = [];
+
+    dataset.forEach( object => {
+        if(!unique_names.includes(object.Name)) {
+            unique_names.push(object.Name);
+            updated_dataset.push({
+                Name: object.Name,
+                Year: parseInt(object.Year),
+                Genre: object.Genre,
+                Platforms: [object.Platform],
+                NA_Sales: Number(object.NA_Sales),
+                EU_Sales: Number(object.EU_Sales),
+                JP_Sales: Number(object.JP_Sales),
+                Other_Sales: Number(object.Other_Sales),
+                Global_Sales: Number(object.Global_Sales)
+            })
+        }
+        else {
+            let game = updated_dataset.find( updated_object => updated_object.Name == object.Name);
+            game.Platforms.push(object.Platform);
+            game.Global_Sales += Number(object.Global_Sales);
+            game.Other_Sales += Number(object.Other_Sales);
+            game.JP_Sales += Number(object.JP_Sales);
+            game.EU_Sales += Number(object.EU_Sales);
+            game.NA_Sales += Number(object.NA_Sales);
+        }
+    })
+
+    updated_dataset.sort( (a, b) => b.Global_Sales - a.Global_Sales);
+
+    console.log(updated_dataset)
+    console.log(dataset);
 }) ()
