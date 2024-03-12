9a174915243ff4ed686d4056ec8bdec20588361b
nelsaj <112635402+nelsaj@users.noreply.github.com>
2024-03-12 10:03:13 +0100

    tooltip fix

--- js/tooltip.js
+++ js/tooltip.js
@@ -4,15 +4,17 @@ export const tooltip = {
     mouseover(event, d) {
         d3.select(".tooltip")
             .style("opacity", 1)
+            .style("z-index", 2)
     },
     mousemove(event, d, textContent) {
         d3.select(".tooltip")
             .html(textContent)
-            .style("left", (event.x + 20) + "px")
-            .style("top", (event.y + "px"))
+            .style("left", (event.pageX + 20) + "px")
+            .style("top", (event.pageY + "px"))
     },
     mouseleave(event, d) {
         d3.select(".tooltip")
             .style("opacity", 0)
+            .style("z-index", 0)
     }
 }
